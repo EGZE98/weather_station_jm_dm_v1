92b55ccd8071b4d95f9f4fe9d372d92684d32ea4
Dawid Masnik <dawidmasnik@gmail.com>
2025-05-08 15:49:19 +0200

    BUZZER files added

---
+++ BUZZER.c
@@ -0,0 +1,6 @@
+/*
+ * BUZZER.c
+ *
+ * Created: 5/8/2025 3:48:11 PM
+ *  Author: dawid
+ */ 
---
+++ BUZZER.h
@@ -0,0 +1,16 @@
+/*
+ * BUZZER.h
+ *
+ * Created: 5/8/2025 3:48:36 PM
+ *  Author: dawid
+ */ 
+
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+
+
+
+
+#endif /* BUZZER_H_ */
